390eda3db3109c928af8df97097d691254262cdf
ericbowman <ericjbowman@gmail.com>
2019-08-21 10:35:48 -0400

    Change recordings layout, backgrounds, + concerto

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,20 +1,52 @@
 const onClickRecordings = function () {
   $('#home-page').hide()
+  $('#students-page').hide()
+  $('#calendar-page').hide()
   $('#recordings-page').show()
   $('body').removeClass('home-background')
+  $('body').removeClass('students-background')
+  $('body').removeClass('calendar-background')
   $('body').addClass('recordings-background')
 }
 
-const onClickHome = function () {
-  $('#home-page').show()
+const onClickStudents = function () {
+  $('#home-page').hide()
   $('#recordings-page').hide()
-  $('body').addClass('home-background')
+  $('#calendar-page').hide()
+  $('#students-page').show()
+  $('body').removeClass('home-background')
   $('body').removeClass('recordings-background')
+  $('body').removeClass('calendar-background')
+  $('body').addClass('students-background')
+}
+
+const onClickCalendar = function () {
+  $('#home-page').hide()
+  $('#recordings-page').hide()
+  $('#students-page').hide()
+  $('#calendar-page').show()
+  $('body').removeClass('home-background')
+  $('body').removeClass('recordings-background')
+  $('body').removeClass('students-background')
+  $('body').addClass('calendar-background')
+}
+
+const onClickHome = function () {
+  $('#recordings-page').hide()
+  $('#calendar-page').hide()
+  $('#students-page').hide()
+  $('#home-page').show()
+  $('body').removeClass('recordings-background')
+  $('body').removeClass('calendar-background')
+  $('body').removeClass('students-background')
+  $('body').addClass('home-background')
 }
 
 const addHandlers = () => {
   $('#recordings').on('click', onClickRecordings)
   $('#home').on('click', onClickHome)
+  $('#calendar').on('click', onClickCalendar)
+  $('#students').on('click', onClickStudents)
 }
 
 module.exports = {
