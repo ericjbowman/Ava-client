7473e59740eba7988fb0438edeb87a5beb2ce86f
ericbowman <ericjbowman@gmail.com>
2019-09-05 00:59:13 -0400

    Create Inputs and create functionality for gigs

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -1,12 +1,23 @@
 'use strict'
 
 const config = require('./config')
-// const store = require('./store')
+const store = require('./store')
 
 const indexGigs = () => {
   return $.ajax({
     url: config.apiUrl + '/gigs',
     method: 'GET'
+  })
+}
+
+const createGig = (data) => {
+  return $.ajax({
+    url: config.apiUrl + '/gigs',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: data
   })
 }
 
@@ -20,5 +31,6 @@ const signIn = function (data) {
 
 module.exports = {
   indexGigs,
+  createGig,
   signIn
 }
